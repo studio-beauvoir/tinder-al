1c0cfe16cdaa7eb4b11ea532f5675e02d2dda8fd
Arthaud Proust <arthaudp33@gmail.com>
2022-01-21 11:46:20 +0100

    fix gitignore

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+//site name
+define('SITE_NAME', 'your-site-name');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+
