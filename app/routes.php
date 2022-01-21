eec30c9f17b8808e93abd472744522065b3b1fcd
Arthaud Proust <arthaudp33@gmail.com>
2022-01-21 17:09:51 +0100

    add routes

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,8 @@
 
 require_once APP_ROOT.'/controllers/UserController.php';
 
-$router->get('/test/:id', function($id){ echo "Bienvenue sur ma homepage !"; }); 
-$router->get('/', function($id){ echo "Bienvenue sur ma homepage !"; }); 
-$router->get('/posts/:id', function($id){ echo "Voila l'article $id"; }); 
+$router->get('/', function(){ echo "Bienvenue sur ma homepage !"; }); 
+
+
+$router->get('/users', function(){ UserController::index(); }); 
+$router->get('/user/:id', function($id){ UserController::show($id); }); 
