c3f2c0164b6bdb4874d1a00416558d557c4d2730
Arthaud Proust <arthaudp33@gmail.com>
2022-01-24 11:53:41 +0100

    add viewport meta

--- app/resources/layout/head.php
+++ app/resources/layout/head.php
@@ -1,6 +1,8 @@
-<html>
+<html lang="fr>
     <head>
         <title>Tinder 22 </title>
         <link rel="stylesheet" href="/css/master.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
